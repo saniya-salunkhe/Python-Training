b51a5b8746680a51dd3dfd4c4e5ccd383225dc0f
Saniya Salunkhe <161126331+saniya-salunkhe@users.noreply.github.com>
2025-08-13 02:39:11 -0700

    Add Regular Expression file.

---
+++ mySQL Regular Expression.sql
@@ -0,0 +1,12 @@
+-- 	Imp for interview 
+-- not null ---> when we fills record that time our record fills their 0 instead of null
+create table example(roll int not null , name varchar(30));
+insert into example values(1,"Rahul"),(2 , "Vishal");
+
+insert into example values(null,"Rajesh"),(4 , "Shreya");
+
+insert into example values(4,"null");
+select * from example;
+
+insert into example (name) values("Vaishali");
+select * from example;
